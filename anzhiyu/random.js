e82b0fc5bef26b28d9075ecee64ed433a08fc4f2
ryahg <1557089079@qq.com>
2025-06-24 18:06:18 +0800

    Site updated: 2025-06-24 18:06:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/07/ryahg-s-first-blog/"];function toRandomPost(){
+var posts=["2025/06/07/ryahg-s-first-blog/","2025/06/24/停更通知📢/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
